8aed6e8d61bd15df2c0fd0b50408ae47b6c32720
Dawid Zielinski <dawid.zielinski@huuugegames.com>
2025-05-07 02:10:27 +0200

    MessageChannel is not defined workaround.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import path from 'path';
+import path from "path";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -8,19 +8,25 @@ import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), sitemap()],
+
   server: { port: 3000 },
+
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve('./src')
+        // --- FIX ↓ ---------------------------------------------------------
+        "react-dom/server": "react-dom/server.edge",
+        // -------------------------------------------------------------------
+        "@": path.resolve("./src")
       }
     }
   },
+
   adapter: cloudflare(),
-  experimental: { session: true },
+
+  experimental: { session: true }   // pamiętaj o KV "SESSION" w wrangler.toml
 });
